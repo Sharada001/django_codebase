7297f31e399476f65c8d4e30bbb36c8d35e1f9ef
Sharada Shehan <sharadashehan6@gmail.com>
2023-07-14 03:34:13 +0530

    urls.py and view.py files were updated added items to database

--- Model_View_Controller/model_view/models.py
+++ Model_View_Controller/model_view/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model) :
+    code = models.IntegerField()
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(decimal_places=2,max_digits=20)
--- Model_View_Controller/model_view/views.py
+++ Model_View_Controller/model_view/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from model_view.models import Food
 
 # Create your views here.
+def sendPrice(request,codeNum):
+    querySet = Food.objects.filter(code=codeNum).values()
+    itemsList = list(querySet)
+    if len(itemsList) == 1:
+        return HttpResponse("Item : "+itemsList[0]['name']+"    Price : "+str(itemsList[0]['price']) )
+    return HttpResponse("Invalid Code !")
