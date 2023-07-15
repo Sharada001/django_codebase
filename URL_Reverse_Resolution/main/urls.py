67f0a20f7cb95a628bb7c830ab382d75bc4de9cd
Sharada Shehan <sharadashehan6@gmail.com>
2023-07-15 10:53:35 +0530

    templates updated

--- URL_Reverse_Resolution/main/urls.py
+++ URL_Reverse_Resolution/main/urls.py
@@ -5,4 +5,5 @@ app_name = "homepage"
 urlpatterns = [
     path("first_view", views.first_view, name="main_view"),
     path("second_view", views.second_view, name="sub_view"),
+    path("view/<slug:text>", views.view_with_path_parameters, name="param_view"),
 ]
--- URL_Reverse_Resolution/main/views.py
+++ URL_Reverse_Resolution/main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.utils.html import escape
 
 def first_view(request):
-    context = {'view_name':'first_view'}
+    context = {'view_name':'Main_View'}
     return render(request,"main/main_template.html",context)
 
 def second_view(request):
-    context = {'view_name': 'second_view'}
+    context = {'view_name': 'Sub_View'}
     return render(request,"main/secondary_template.html",context)
+
+def view_with_path_parameters(request,text):
+    context = {'view_name': 'Param_View', 'text': escape(text)}
+    return render(request,"main/third_template.html",context)
