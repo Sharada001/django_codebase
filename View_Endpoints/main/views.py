31276ac4317a1c8dca19542a2670e5356c79b66d
Sharada Shehan <sharadashehan6@gmail.com>
2023-07-15 10:04:02 +0530

    XSS escaped

--- View_Endpoints/main/views.py
+++ View_Endpoints/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from django.utils.html import escape
 
 # Create your views here.
 def function_based_view(request):
@@ -9,7 +9,7 @@ def function_based_view(request):
     return render(request, "main/template_with_data_rendering.html", context)
 
 def function_based_view_with_path_parameters(request,text):
-    context = {'render_name': 'function_based_view', 'text':text}
+    context = {'render_name': 'function_based_view', 'text':escape(text)}
     return render(request, "main/template_with_more_data_rendering.html", context)
 
 
@@ -20,5 +20,5 @@ class class_based_view(View):
 
 class class_based_view_with_path_parameters(View):
     def get(self,request,text):
-        context = {'render_name': 'class_based_view', 'text':text}
+        context = {'render_name': 'class_based_view', 'text':escape(text)}
         return render(request, "main/template_with_more_data_rendering.html", context)
