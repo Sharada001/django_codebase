a61148aaf23a64b0e77088863b7acd2308d507c3
Sharada Shehan <sharadashehan6@gmail.com>
2023-07-15 12:30:54 +0530

    derived template added

--- Django_Template_Language/main/urls.py
+++ Django_Template_Language/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "mainPage"
 urlpatterns = [
     path('template', views.render_template, name="template_view"),
+    path('derived_template', views.render_derived_template, name="derived_template_view"),
 ]
 
--- Django_Template_Language/main/views.py
+++ Django_Template_Language/main/views.py
@@ -5,3 +5,10 @@ def render_template(request):
                'list_of_people':["Jack", "John", "Harry", "Peter"],
                'productivity': 20}
     return render(request,'main/main_template.html',context)
+
+def render_derived_template(request):
+    context = {'sp_Person':{'name':'Sam','role':'Manager','id':10},
+               'list_of_people':["Steve", "Henry", "Thomas", "Kane", "Bill"],
+               'productivity': 80,
+               'average_salary':50000}
+    return render(request,'main/derived_template.html', context)
