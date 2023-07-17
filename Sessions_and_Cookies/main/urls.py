4a1648d56120ef2f7337b0bf130f7092de4984c1
Sharada Shehan <sharadashehan6@gmail.com>
2023-07-18 00:05:20 +0530

    sessions and cookies APIs created

--- Sessions_and_Cookies/main/urls.py
+++ Sessions_and_Cookies/main/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-
+urlpatterns = [
+    path('get_view_count', views.get_view_count),
+    path('cookies', views.print_cookies),
+]
--- Sessions_and_Cookies/main/views.py
+++ Sessions_and_Cookies/main/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
+def get_view_count(request):
+    request.session['view_count'] = request.session.get('view_count',0) + 1
+    return HttpResponse("Your view count is "+str(request.session['view_count']))
+
+def print_cookies(request):
+    if 'code' in request.COOKIES:
+        code = request.COOKIES['code']
+        return HttpResponse('Your code is '+str(code))
+    code = random.randint(100000,200000)
+    resp = HttpResponse('Welcome to our web. Your code is '+str(code))
+    resp.set_cookie('code', code)
+    return resp
 
 
